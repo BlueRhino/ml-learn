24f83225c251df02b83fe33428dfbb67fceb5860
bluerhino <nie_kl@126.com>
2017-12-24 22:24:48 +0800

    implement basic pla

--- pla/basic_pla.py
+++ pla/basic_pla.py
@@ -1,15 +1,63 @@
 import numpy as np
+from numpy import where
+from sklearn.datasets import make_classification
 
 
 def train(train_data, learn_rate):
-    # TODO:没有校验输入数据
+    # TODO:没有校验输入数据，也没有确定训练集是线程可分的
     train_array = np.array(train_data)
     data_len = len(train_data)
-    dimension = data_len - 1
+    dimension = len(train_data[0]) - 1
     has_error = True
-    w0 = 0.0
-    b0 = 0.0
+    w = np.zeros(dimension)
+    b = 0.5
     while has_error:
+        has_error = False
         for i in range(data_len):
             x = train_array[i, 0:-1]
             yi = train_array[i, -1]
+            res = inner_function(w, x, b)
+            inner_res = yi * res
+            if inner_res <= 0:
+                w = w + learn_rate * yi * x
+                b = b + learn_rate * yi
+                has_error = True
+    return w, b
+
+
+def inner_function(w, x, b):
+    return np.inner(w, x) + b
+
+
+def plot(train_data, w, b):
+    # TODO:只能画二维图，并且没有判断输入数据的正确性
+    import matplotlib.pyplot as plt
+    train_data = np.array(train_data)
+    positive_point_index_arr = where(train_data[:, -1] == 1)
+    opposite_point_index_arr = where(train_data[:, -1] == -1)
+    for tmp in positive_point_index_arr[0]:
+        plt.scatter(train_data[tmp][0], train_data[tmp][1], c='r')
+    for tmp in opposite_point_index_arr[0]:
+        plt.scatter(train_data[tmp][0], train_data[tmp][1], c='g')
+    plt.plot([0, (-b) / w[1]], [1, (-b - w[0]) / w[1]])
+    plt.show()
+
+
+def main():
+    train_data = []
+    samples = make_classification(n_samples=100, n_features=2, n_redundant=0, n_informative=1,
+                                  n_clusters_per_class=1, flip_y=-1)
+    for index, label in enumerate(samples[1]):
+        if label > 0:
+            train_data.append(np.insert(samples[0][index], len(samples[0][index]), values=1))
+        else:
+            train_data.append(np.insert(samples[0][index], len(samples[0][index]), values=-1))
+    l_rate = 1
+    w, b = train(train_data, l_rate)
+    plot(train_data, w, b)
+    print(w)
+    print(b)
+
+
+if __name__ == '__main__':
+    main()
